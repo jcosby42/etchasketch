d44560e456705b067b3e5de3fb227f1e835b0197
jcosby42 <johncosby42@hotmail.com>
2023-03-30 11:45:43 -0400

    Draw borders

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 //Use JS to create the grid with given inputs
 //Right now: Hard program in the sizes; later: take user input for sizes
 function createGrid(){
-    let width = 8, height = 8;
+    let width = 16, height = 16;
     for (let i = 0; i < height; i++){
         const newDiv = document.createElement("div");
         for (let j = 0; j < width; j++){
             const newSpan = document.createElement("span");
-            const newContent = document.createTextNode(` ${i},${j} -`);
+            const newContent = document.createTextNode("*");
             newSpan.appendChild(newContent);
             newDiv.appendChild(newSpan);
         }
