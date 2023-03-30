99493728ea4067f39426a1abe48b03a55ef7fb0e
jcosby42 <johncosby42@hotmail.com>
2023-03-30 18:16:16 -0400

    Add reset button

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 //Use JS to create the grid with given inputs
 //Right now: Hard program in the sizes; later: take user input for sizes
-function createGrid(width = 16, height = 16){
+function createGrid(width = 100, height = 100){
     for (let i = 0; i < height; i++){
         const newDiv = document.createElement("div");
         newDiv.classList.add("row");
@@ -18,3 +18,13 @@ function createGrid(width = 16, height = 16){
 };
 
 createGrid();
+
+let btn = document.querySelector("button");
+btn.addEventListener('click', () => {
+    let grid = document.getElementsByClassName("element");
+    for (let element of grid){
+        if (element.classList.contains("hovered")){ 
+            element.classList.remove("hovered");
+        }
+    }
+});
