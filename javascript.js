a7285fa286c2a090547e5c304e6831d1c94d1e1a
jcosby42 <johncosby42@hotmail.com>
2023-03-30 18:03:30 -0400

    Add event listener to divs and change background on hover

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
 //Use JS to create the grid with given inputs
 //Right now: Hard program in the sizes; later: take user input for sizes
-function createGrid(){
-    let width = 16, height = 16;
+function createGrid(width = 16, height = 16){
     for (let i = 0; i < height; i++){
         const newDiv = document.createElement("div");
         newDiv.classList.add("row");
         for (let j = 0; j < width; j++){
-            const newSpan = document.createElement("div");
-            newSpan.classList.add("element");
-            const newContent = document.createTextNode("*");
-            newSpan.appendChild(newContent);
-            newDiv.appendChild(newSpan);
+            const newElement = document.createElement("div");
+            newElement.classList.add("element");
+            newElement.addEventListener('mouseover', ()=>{
+                newElement.classList.add("hovered");
+            })
+            newDiv.appendChild(newElement);
         }
         const canvas = document.getElementById("canvas");
         canvas.appendChild(newDiv);
     }
-}
+};
 
 createGrid();
